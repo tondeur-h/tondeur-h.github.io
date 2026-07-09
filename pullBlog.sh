1a26fb09d2513dc205e7f832c4cafe6d043bd3de
tondeur-h@ctondeur-h-Optiplex-3040 <tondeur-h@ch-valenciennes.fr>
2026-07-09 07:55:14 +0200

    Auto-commit du 2026-07-09 07:55:14

---
+++ pullBlog.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# URL GitHub avec login + token (variables d'environnement)
+REPO_URL="https://${GIT_LOGIN}:${GIT_TOKEN}@github.com/tondeur-h/tondeur-h.github.io.git"
+
+# Récupération des dernières modifications
+git pull "$REPO_URL" main
